24c46f81664bb86c48404fefa2098edd754db605
Moyo Oyegunle <moyo.oyegunle@yahoo.com>
2020-05-19 23:26:32 -0400

    update code

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,34 +10,45 @@ from config import Config
 
 
 
+def refresh_db(url):
+    form = PostForm()
+    stored_values={}
+    db_value = {}
+    db_time = {}
+    db_remove = {}
+
+    url=url.replace("/index","")
+    stored_values = KeyStore.query.all()
+    for val in stored_values:
+        db_time.update({val.key:val.timestamp})
+        db_value.update({val.key:val.value})
+        db_remove.update({val.key:"{}{}?key={}".format(url,url_for('main.db_remove'),val.key)})
+    return [db_value, db_time, db_remove]
+
+
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
     form = PostForm()
-    message = "Messages: "
+    fullurl = ""
     db_value = {}
     db_time = {}
+    db_remove = {}
     configs = Config.myclassvariables()
 
-    stored_values={}
+    
     if not Config.DB_INIT:
         try:
             db.create_all()
         except:
             pass
         Config.DB_INIT = True;
-    stored_values = KeyStore.query.all()
-    for val in stored_values:
-        db_time.update({val.key:val.timestamp})
-        db_value.update({val.key:val.value})
-
-        print(val.key)
-        print(val.value)
-        print(val.timestamp)
-
+   
+    
     if form.validate_on_submit():
         key = request.form["key"]
         value = request.form["value"]
+        fullurl = request.base_url
         insertime = datetime.utcnow()
 
         #Check if Key Already Exists
@@ -45,12 +56,11 @@ def index():
             temp = KeyStore(timestamp=insertime, key=request.form["key"], value=request.form["value"])
             db.session.add(temp)
             db.session.commit()
-            message = "Messages: Key {} Added".format(request.form["key"])
         else:
             flash('Value already exists for key {}'.format(key))
-            message = "Key {} Added".format(request.form["key"])
-            return render_template('index.html', form=form, configs=configs,message=message,
-            db_value=db_value,db_time=db_time)
+            temp_db = refresh_db(fullurl)
+            return render_template('index.html', form=form, configs=configs,
+            db_value=temp_db[0],db_time=temp_db[1], db_remove=temp_db[2])
 
         #if value_remove is not None:
             #Check if key is in DB
@@ -61,13 +71,24 @@ def index():
         #value = KeyStore.query.filter_by(value=form.value.data).first()
 
         #return redirect('/index')
-        return render_template('index.html', form=form, configs=configs,message=message,
-            db_value=db_value,db_time=db_time)
+        temp_db = refresh_db(fullurl)
+        return render_template('index.html', form=form, configs=configs,
+            db_value=temp_db[0],db_time=temp_db[1], db_remove=temp_db[2])
     
-    return render_template('index.html', form=form, configs=configs,message=message,
-            db_value=db_value,db_time=db_time)
+    temp_db = refresh_db(fullurl)
+    return render_template('index.html', form=form, configs=configs,
+            db_value=temp_db[0],db_time=temp_db[1], db_remove=temp_db[2])
 
-@bp.route('/Database', methods=['GET', 'POST'])
-def db_view():
-    return """  
-    """
+
+@bp.route('/remove', methods=['GET', 'POST'])
+def db_remove():
+    value_remove=request.args.get("key")
+    if value_remove is not None:
+        #Check if key is in DB
+        tempkey = KeyStore.query.filter_by(key=value_remove).first()
+        if tempkey is not None:
+            db.session.delete(tempkey)
+            db.session.commit()
+    return redirect('/index')
+    
+    
--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class KeyStore(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     key = db.Column(db.String(64), index=True, unique=True)
-    value = db.Column(db.String(120), index=True, unique=True)
+    value = db.Column(db.String(120), index=True, unique=False)
     
     def __repr__(self):
         return '<Key {}>'.format(self.value) 
